736584c064b643405b80b78b01f038156dd6977e
Rayhan K <166932170+rayhankimi@users.noreply.github.com>
2024-08-31 15:02:28 +0800

    Implement Navbar using tailwind

---
+++ src/components/Navbar/NavList.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Typography } from "@material-tailwind/react";
+
+export function NavList(): JSX.Element {
+    return (
+        <ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+            <Typography
+                as="li"
+                variant="small"
+                color="blue-gray"
+                className="p-1 font-normal"
+            >
+                <a href="#" className="flex items-center">
+                    Home
+                </a>
+            </Typography>
+            <Typography
+                as="li"
+                variant="small"
+                color="blue-gray"
+                className="p-1 font-normal"
+            >
+                <a href="#" className="flex items-center">
+                    About
+                </a>
+            </Typography>
+            <Typography
+                as="li"
+                variant="small"
+                color="blue-gray"
+                className="p-1 font-normal"
+            >
+                <a href="#" className="flex items-center">
+                    Portfolio
+                </a>
+            </Typography>
+            <Typography
+                as="li"
+                variant="small"
+                color="blue-gray"
+                className="p-1 font-normal"
+            >
+                <a href="#" className="flex items-center">
+                    Contacts
+                </a>
+            </Typography>
+        </ul>
+    );
+}
